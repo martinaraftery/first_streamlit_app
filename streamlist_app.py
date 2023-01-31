9b237ec25150f3fad9fb0d928fcb64a993b714ca
MartinaRaftery <55628150+martinaraftery@users.noreply.github.com>
2023-01-31 12:28:35 +0000

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -63,7 +63,7 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
   
-streamlit.stop()
+#streamlit.stop()
 
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
